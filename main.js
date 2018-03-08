a7dd46258dab98622ce8ba27f0c2b5b2734f6053
Jamie Bort <jamiebort@gmail.com>
2018-03-08 16:59:40 -0500

    Refactoring code so that I learn this material better.

--- main.js
+++ main.js
@@ -3,31 +3,35 @@
 // Formatting the html
 
 var body = document.querySelector("body");
-
 var h1 = document.createElement("h1");
+var header = document.createElement("header")
 var recipe = document.createTextNode("Recipe");
+
+
+
 h1.appendChild(recipe);
-body.appendChild(h1);
+body.appendChild(header);
+header.appendChild(h1);
 
 var input = document.createElement("input");
 input.setAttribute("placeholder", "Type an ingredient");
-body.appendChild(input);
-
+header.appendChild(input);
 
 var buttonSearch = document.createElement("button");
 var searchText = document.createTextNode("Search For Recipes");
 buttonSearch.appendChild(searchText);
-body.appendChild(buttonSearch);
+header.appendChild(buttonSearch);
 
 var buttonClear = document.createElement("button");
 var clearText = document.createTextNode("Reset / Clear Search");
 buttonClear.appendChild(clearText);
-body.appendChild(buttonClear);
+header.appendChild(buttonClear);
 
+var section = document.createElement("section")
+body.appendChild(section);
 
-// Element.setAttribute(name, value);
 // h1.innerHTML = "Recipe"; // NOTE: 'createTextNode' is favorable to 'innerHTML': https://stackoverflow.com/questions/13122760/is-there-any-major-difference-between-innerhtml-and-using-createtextnode-to-fill
-body.appendChild(h1);
+// body.appendChild(h1);
 
 
 
