2be173d0ce8e419f7446cc104b81fdaa3eb6715b
Jamie Bort <jamiebort@gmail.com>
2018-03-09 14:46:01 -0500

    Refactoring code so that I learn this material better.

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ buttonSearch.addEventListener('click', function() {
   section.innerHTML = "The event listener is working."
 
   // TODO: HAVE THE INFO ENTERED INTO THE SEARCH FIELD POPLULATE THE END OF THIS URL:
-  fetch("http://www.recipepuppy.com/api/")
+  fetch(`https://crossorigin.me/http://www.recipepuppy.com/api/?i=${input.value}`) // NOTE: using the following in the URL of the fetch: https://crossorigin.me/ // NOTE: use ticks rather than signle quotes or double quotes in the URL in order to use the object literal notation '${}'.
     // Data is fetched and we get a promise.
     .then(
       // The promise returns a response from the server.
@@ -50,7 +50,23 @@ buttonSearch.addEventListener('click', function() {
           console.log("Here is the data:", data);
 
           // for each item that comes back create a div:
-            document.createElement("div");
+          for (var i = 0; i < data.results.length; i++) {
+            var div = document.createElement("div");
+            section.appendChild(div);
+            div.innerHTML =
+              `<li><img id="image" src=${data.results[i].thumbnail}></li>
+              <h2>${data.results[i].title}</h2>
+               <li><a href=${data.results[i].href}>${data.results[i].href}</a></li>`
+          }
+
+          // var div = document.createElement("div");
+          // section.appendChild(div);
+          // console.log(data.title);
+          // console.log(data.results);
+          // console.log(data.results[0]);
+          // console.log(data.results[0].title);
+          // console.log(data.results[0].href);
+
         });
       }
     )
