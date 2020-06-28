620f31dd540a0cb80b149a433a6c67ec5002a1df
Jamie Bort <jamiebort@gmail.com>
2020-06-28 17:59:25 -0400

    Modified the files from the directory of the same name under the LearningDirectory. See first line of README.md file.

--- main.js
+++ main.js
@@ -47,7 +47,7 @@ function searchFunction(){
   console.log(document.getElementById('searchFieldID').value);
   searchPhrase = document.getElementById('searchFieldID').value;
 
-  fetch("http://recipepuppyproxy.herokuapp.com/api/?q=" + searchPhrase)
+  fetch("https://recipepuppyproxy.herokuapp.com/api/?q=" + searchPhrase)
   .then(
     function(response){
       console.log("The response status is: ", response.status);
@@ -72,7 +72,6 @@ function searchFunction(){
         // TODO: add attributes so that the site conforms to a11y.
         // TODO: have a default image for when one is not available.
         if(data.results[i].thumbnail==""){
-          console.log("Its empty");
           image.setAttribute("src", "./images/mcdonalds.jpg");
         } else {
           image.setAttribute("src", data.results[i].thumbnail);
